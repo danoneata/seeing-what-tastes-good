2a410413de04ead1e14a12c679e87d484ff3c056
Dan Oneata <dan.oneata@gmail.com>
2025-07-16 12:25:32 +0300

    MINOR Show results

--- probing_norms/scripts/show_results_per_feature_norm_v2.py
+++ probing_norms/scripts/show_results_per_feature_norm_v2.py
@@ -132,26 +132,26 @@ def main():
             _, model = sort_by.split()
             return -results_2[model][1][concepts.index(concept)]
 
-    # concepts_order = sorted(concepts, key=sort_func)
+    concepts_order = sorted(concepts, key=sort_func)
 
-    # for i, concept in enumerate(concepts_order):
-    #     cols = st.columns(1 + num_models)
-    #     label_str = "+" if concept in feature_to_concepts[feature] else "-"
-    #     c = concepts.index(concept)
-    #     cols[0].markdown("#{} · {} · {}".format(i, concept, label_str))
-    #     cols[0].image(str(get_image_path(concept)))
-    #     for i in range(num_models):
-    #         model = MODELS[i]
-    #         model_name = FEATURE_NAMES[model]
-    #         true, pred = results_2[model]
-    #         t = true[c]
-    #         p = pred[c]
-    #         b = p >= 0.5
-    #         # pred_str = "+" if p else "-"
-    #         is_correct_str = "✓" if t == b else "✗"
-    #         cols[i + 1].markdown("{} · pred: {:.1f} · is correct: {}".format(model_name, p, is_correct_str))
+    for i, concept in enumerate(concepts_order):
+        cols = st.columns(1 + num_models)
+        label_str = "+" if concept in feature_to_concepts[feature] else "-"
+        c = concepts.index(concept)
+        cols[0].markdown("#{} · {} · {}".format(i, concept, label_str))
+        cols[0].image(str(get_image_path(concept)))
+        for i in range(num_models):
+            model = MODELS[i]
+            model_name = FEATURE_NAMES[model]
+            true, pred = results_2[model]
+            t = true[c]
+            p = pred[c]
+            b = p >= 0.5
+            # pred_str = "+" if p else "-"
+            is_correct_str = "✓" if t == b else "✗"
+            cols[i + 1].markdown("{} · pred: {:.1f} · is correct: {}".format(model_name, p, is_correct_str))
 
-    # return 
+    return 
 
     predicted_concepts = [
         concept
@@ -173,21 +173,21 @@ def main():
 
     st.markdown("---")
 
-    # cols = st.columns(1 + num_models)
-    # cols[0].markdown("## Concept")
-    # for i in range(num_models):
-    #     model = MODELS[i]
-    #     model_name = FEATURE_NAMES[model]
-    #     cols[i + 1].markdown("## {} ({:.1f})".format(model_name, scores[model]))
+    cols = st.columns(1 + num_models)
+    cols[0].markdown("## Concept")
+    for i in range(num_models):
+        model = MODELS[i]
+        model_name = FEATURE_NAMES[model]
+        cols[i + 1].markdown("## {} ({:.1f})".format(model_name, scores[model]))
 
-    # for c in predicted_concepts:
-    #     cols = st.columns(1 + num_models)
-    #     cols[0].image(str(get_image_path(c)))
-    #     cols[0].markdown("{}".format(c))
-    #     for i in range(num_models):
-    #         cols[i + 1].markdown(get_pred_type(results_tp_fp_fn[MODELS[i]], c))
+    for c in predicted_concepts:
+        cols = st.columns(1 + num_models)
+        cols[0].image(str(get_image_path(c)))
+        cols[0].markdown("{}".format(c))
+        for i in range(num_models):
+            cols[i + 1].markdown(get_pred_type(results_tp_fp_fn[MODELS[i]], c))
 
-    # return
+    return
 
     r = [
         {
